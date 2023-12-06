59e4f0402c62473b50ebf038c0428cff4e929eaa
ContardiC <c.contardi@icloud.com>
2023-12-06 13:21:59 +0100

    first tables added

---
+++ sql/dati.sql
@@ -0,0 +1,41 @@
+INSERT INTO autori (nome, cognome)
+VALUES 
+('Gabriel', 'García Márquez'),
+('Ernest', 'Hemingway'),
+('Jane', 'Austen'),
+('Mark', 'Twain'),
+('George', 'Orwell'),
+('Virginia', 'Woolf'),
+('Oscar', 'Wilde'),
+('Charles', 'Dickens'),
+('Fyodor', 'Dostoevsky'),
+('Leo', 'Tolstoy');
+
+
+
+INSERT INTO editori (nome, luogo)
+VALUES 
+('Mondadori', 'Milano, Italia'),
+('Einaudi', 'Torino, Italia'),
+('Rizzoli', 'Milano, Italia'),
+('Bompiani', 'Milano, Italia'),
+('Feltrinelli', 'Milano, Italia'),
+('Adelphi', 'Milano, Italia'),
+('Laterza', 'Bari, Italia'),
+('Garzanti', 'Milano, Italia'),
+('Zanichelli', 'Bologna, Italia'),
+('Giunti', 'Firenze, Italia');
+
+
+INSERT INTO generi (genere)
+VALUES 
+('Romanzo'),
+('Poesia'),
+('Fantascienza'),
+('Fantasy'),
+('Giallo'),
+('Thriller'),
+('Biografia'),
+('Saggio'),
+('Storico'),
+('Horror');
---
+++ sql/ddl.sql
@@ -0,0 +1,19 @@
+CREATE TABLE autori(
+    id INT AUTO_INCREMENT,
+    nome VARCHAR(255),
+    cognome VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE editori(
+    id INT AUTO_INCREMENT,
+    nome VARCHAR(255),
+    luogo VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE generi(
+    id INT AUTO_INCREMENT,
+    nome VARCHAR(60),
+    PRIMARY KEY(id)
+);
