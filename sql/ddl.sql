64d6af06ba82c733c436f322841231d3b72c3050
Carlo Contardi <c.contardi@spacecoding.it>
2023-12-06 17:34:53 +0100

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,27 +1,27 @@
 CREATE TABLE autori(
     id INT AUTO_INCREMENT,
-    nome VARCHAR(255),
-    cognome VARCHAR(255),
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
     nazione CHAR(2),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE editori(
     id INT AUTO_INCREMENT,
-    nome VARCHAR(255),
-    luogo VARCHAR(255),
+    nome VARCHAR(255) NOT NULL,
+    luogo VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE generi(
     id INT AUTO_INCREMENT,
-    nome VARCHAR(60),
+    nome VARCHAR(60) NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE libri(
     id INT AUTO_INCREMENT,
-    titolo TEXT,
+    titolo TEXT NOT NULL,
     id_autore INT,
     id_editore INT,
     id_genere INT,
@@ -33,8 +33,8 @@ CREATE TABLE libri(
 
 CREATE TABLE utenti(
     id INT AUTO_INCREMENT,
-    nome VARCHAR(255),
-    cognome VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(64),
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(64) NOT NULL,
 );
