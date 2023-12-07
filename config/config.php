323799e01a1458d0d049a9fea1896113667204f0
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-12-07 11:10:19 +0100

    config.php added

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+define('DEV', true);                    // In sviluppo o online?
+define('DOC_ROOT', '/public/');         // Percorso doc root del sito 
+define('ROOT_FOLDER', 'public');        // Nome della cartella root
+// Impostazioni del database
+$type = 'mysql';                        // Tipo di database
+$server = 'localhost';                  // Server su cui si trova il database
+$db = 'library';                        // Nome del database
+$username = 'root';                     // Nome utente
+$password = 'root';                     // Password
+
+// TODO:   Impostazioni per l'upload dei file
+
+?>
