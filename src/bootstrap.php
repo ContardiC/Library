1620a41f26cfd2915b36d78b4f8dc0ee36d731b7
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-12-07 11:35:52 +0100

    bootstrap.php added

---
+++ src/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+define('APP_ROOT', dirname(__FILE__, 2));            // Root dell' applicazione (2 livelli sopra)
+require APP_ROOT . '/config/config.php';             // Dati di configurazione 
+require APP_ROOT . '/src/functions.php';             // Funzioni 
+
+// TODO: autoload classi 
+
+if(DEV !== true){
+    set_exception_handler('handle_exception');          // Imposta il gestore delle eccezzioni
+    set_error_handler('handle_error');                  // Imposta il gestore degli errori
+    register_shutdown_function('handle_shutdown');      // Imposta il gestore del shutdwon
+}
+$conn = new mysqli($erver,$usename,$password);
+if($conn->error){
+    echo 'Errore di connessione ' . $conn->error;
+}else{
+    // echo 'test connessione ok';
+}
+unset($server, $username, $password);                   // Rimuove i dati di connessione al db
