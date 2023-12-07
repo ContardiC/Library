c08d323c6bad41ab51efa0ca666b64dcf6f767fb
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-12-07 16:52:34 +0100

    Utente class updated

--- src/classes/Utente.php
+++ src/classes/Utente.php
@@ -1,28 +1,32 @@
-<?php 
-class Utente{
+<?php
+class Utente
+{
     private string $nome;
     private string $cognome;
     private string $email;
     private string $password;
 
-    public function __construct($nome, $cognome, $email, $password){
+    public function __construct($nome, $cognome, $email, $password)
+    {
         $this->nome = $nome;
         $this->cognome = $cognome;
         $this->email = $email;
         $this->password = $password;
     }
-    public function getCognome(){
+    public function getCognome(): string
+    {
         return $this->cognome;
     }
-    public function getNome(){
+    public function getNome(): string
+    {
         return $this->nome;
     }
-    public function getEmail(){
+    public function getEmail(): string
+    {
         return $this->email;
     }
-    public function getPassword(){
+    public function getPassword(): string
+    {
         return $this->password;
     }
-    
 }
-?>
