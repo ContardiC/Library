0d264efbaf647b4b8d71cf11e8fd4a97dbc6fa0c
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-12-11 17:25:08 +0100

    session added

--- public/login.php
+++ public/login.php
@@ -8,11 +8,11 @@ include '../includes/header.php';
             <h1 class="h3 mb-3 fw-normal">Login</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
                 <label for="email">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
                 <label for="password">Password</label>
             </div>
             <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
--- src/add-user.php
+++ src/add-user.php
@@ -10,14 +10,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // echo "$nome $cognome $email $password";
     // TODO: controllare se l'utente è già registrato 
-    
+
     $sql = "INSERT INTO utenti(nome, cognome, email, password)
     VALUES('$nome','$cognome','$email','$password')";
 
     if ($conn->query($sql) === TRUE) {
         header("Location: ../public/login.php");
         exit;
-
     } else {
         echo "Errore " . $conn->error;
     }
