2932a90db9f82146d3a0b38ffafec8572093c094
ContardiC <c.contardi@icloud.com>
2023-12-12 12:54:00 +0100

    success login redirect updated

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,7 +4,7 @@
   <div class="container-fluid">
     <a class="navbar-brand">Library</a>
     <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search"/>
       <button class="btn btn-outline-success" type="submit">Ricerca</button>
     </form>
   </div>
