67cf34faaf0c5256efbeb75743018e61fd2db493
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-12-12 17:10:43 +0100

    logout function implemented

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,6 +7,9 @@
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search"/>
       <button class="btn btn-outline-success" type="submit">Ricerca</button>
     </form>
+    <a href="logout.php"> Logout
+    <img src="../public/img/door-closed.svg" height="30"/>
+    </a>
   </div>
 </nav>
 <?php
