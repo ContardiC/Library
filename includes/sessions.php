5d573f5a2a1b75b8c2acfb61157234a1eba8e842
ContardiC <c.contardi@icloud.com>
2023-12-13 12:37:18 +0100

    pretty formatted

--- includes/sessions.php
+++ includes/sessions.php
@@ -1,29 +1,26 @@
 <?php
 session_start();
+
 $logged_in = $_SESSION['logged_in'] ?? false;
 
-function require_login($logged_in){
-    if($logged_in === false){
+function require_login($logged_in)
+{
+    if ($logged_in === false) {
         header("Location: login.php");
         exit;
     }
 }
 
-function login($email){
+function login($email)
+{
     session_regenerate_id(true); // aggiorno l'id della sessione
     $_SESSION['logged_in'] = true;
     $_SESSION['email'] = $email;
 }
-function logout(){    
-    $_SESSION =[];      // elimino contenuto dell'array
+function logout()
+{
+    $_SESSION = [];      // elimino contenuto dell'array
     $params = session_get_cookie_params(); // leggo i parametri del cookie di sessione
-    setcookie('PHPSESSID','',time()-3600, $params['path'],$params['domain'],$params['secure'],$params['httponly']);
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
     session_destroy();
-
-
 }
-
-
-
-
-?>
