c2a47ce064061dbf7a9159c0032c1ef5657bd107
ContardiC <c.contardi@icloud.com>
2023-12-14 12:02:41 +0100

    index page updated

--- public/index.php
+++ public/index.php
@@ -1,16 +1,49 @@
 <?php
 include '../includes/sessions.php';
 require_login($logged_in);
+include '../config/database-connection.php';
 include '../includes/header.php';
 include '../includes/navbar.php';
 include '../includes/footer.php';
-?>
 
+$email = $_SESSION['email'];
+echo "Ciao $email";
+$sql = "SELECT id FROM utenti WHERE email LIKE '$email'";
+$res = $conn->query($sql);
+
+if($res->num_rows>0){
+    $row = $res->fetch_assoc();
+    $id = $row['id'];
+    $sql = "SELECT l.titolo AS titolo , p.data_prestito AS datap FROM libri l, prestiti p, utenti u
+    WHERE p.id_utente = u.id AND
+    p.id_libro = l.id AND u.id = $id";
+    $res = $conn->query($sql);
+    if($res->num_rows>0){    
+?>
+<div class="card" style="width: 18rem;">
+  <div class="card-header">
+    Libri in prestito 
+  </div>
+  <ul class="list-group list-group-flush">
+  <?php
+  while($row = $res->fetch_assoc()){
+    echo "<li class='list-group-item'>{$row['titolo']}</li>";
+    echo "<li class='list-group-item'>{$row['datap']}</li>";
+  }
+  
+  ?>
+  </ul>
+</div>
 
 
 
 
 
 <?php
+    
+    }
+}else{
+    echo "Non hai libri in prestito";
+}
 include '../includes/footer.php';
 ?>
