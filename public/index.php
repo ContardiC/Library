da4072c504e3a7e01510164dab32b16e58994128
ContardiC <c.contardi@icloud.com>
2023-12-14 12:12:29 +0100

    index page updated

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@ include '../config/database-connection.php';
 include '../includes/header.php';
 include '../includes/navbar.php';
 include '../includes/footer.php';
+    
+    echo "<div class='container'>";
+    $email = $_SESSION['email'];
+    echo "Ciao $email";
+    $sql = "SELECT id FROM utenti WHERE email LIKE '$email'";
+    $res = $conn->query($sql);
 
-$email = $_SESSION['email'];
-echo "Ciao $email";
-$sql = "SELECT id FROM utenti WHERE email LIKE '$email'";
-$res = $conn->query($sql);
-
-if($res->num_rows>0){
     $row = $res->fetch_assoc();
     $id = $row['id'];
     $sql = "SELECT l.titolo AS titolo , p.data_prestito AS datap FROM libri l, prestiti p, utenti u
@@ -41,9 +41,10 @@ if($res->num_rows>0){
 
 <?php
     
-    }
-}else{
-    echo "Non hai libri in prestito";
+    }else{
+        // TODO: sistemare le parentesi graffe
+        echo "<h1>Non hai libri in prestito </h1>";
 }
+echo "</div>";
 include '../includes/footer.php';
 ?>
